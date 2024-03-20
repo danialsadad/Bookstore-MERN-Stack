1edd128069ff69d0205a82515f4f3c5d290301ea
Danial Sadad <110252752+danialsadad@users.noreply.github.com>
2024-03-21 04:57:42 +0600

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const port = 5555;
 
-export const mongoDBURL = 'mongodb+srv://danialsadad042:admin@bookstore.li6cvlc.mongodb.net/books-collections?retryWrites=true&w=majority&appName=BookStore'
+export const mongoDBURL = 'mongodb+srv://danialsadad042:root@bookstore.li6cvlc.mongodb.net/books-collections?retryWrites=true&w=majority&appName=BookStore'
